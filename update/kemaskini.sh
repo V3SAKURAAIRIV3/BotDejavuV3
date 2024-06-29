6539408b1267783b66c3b63163ce1e79469ea808
SakuraV3 <109644626+V3SAKURAAIRIV3@users.noreply.github.com>
2024-06-30 02:48:59 +0800

    Add files via upload

---
+++ update/kemaskini.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# ======================================
+# Autoscript Modded By SakuraV3 2024
+# ======================================
+
+echo -e cheking update
+sleep 1
+#hapus
+#rm -f /usr/bin/setting
+#rm -f /usr/bin/menu
+#rm -f /usr/bin/usernew
+#rm -f /usr/bin/menu-vless
+#download
+#wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/V3SAKURAAIRIV3/BotDejavuV3/main/menu/menu.sh" && chmod +x /usr/bin/menu
+#wget -q -O /usr/bin/setting "https://raw.githubusercontent.com/V3SAKURAAIRIV3/BotDejavuV3/main/menu/setting.sh"
+#wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/V3SAKURAAIRIV3/BotDejavuV3/main/usernew.sh"
+#wget -q -O /usr/bin/menu-vless "https://raw.githubusercontent.com/V3SAKURAAIRIV3/BotDejavuV3/main/menu-vless.sh"
+#echo "*/3 * * * * root bot" >> /etc/crontab
+#izin
+#chmod 777 /usr/bin/usernew
+#chmod 777 /usr/bin/menu
+#chmod 777 /usr/bin/setting
+#chmod 777 /usr/bin/menu-vless
+#rm -rf kemaskini.sh
+#menu
